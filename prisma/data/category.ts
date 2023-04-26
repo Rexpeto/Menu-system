61a445fb3ece6a87346ff7f71c502be00d923672
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-26 13:26:23 -0400

    Data for Category

---
+++ prisma/data/category.ts
@@ -0,0 +1,28 @@
+const categorySeed = [
+    {
+        icon: "SiBuymeacoffee",
+        name: "Café",
+    },
+    {
+        icon: "FaHamburger",
+        name: "Hamburguesas",
+    },
+    {
+        icon: "FaPizzaSlice",
+        name: "Pizzas",
+    },
+    {
+        icon: "GiDonut",
+        name: "Donas",
+    },
+    {
+        icon: "GiCakeSlice",
+        name: "Pasteles",
+    },
+    {
+        icon: "MdCookie",
+        name: "Galletas",
+    },
+];
+
+export { categorySeed };
