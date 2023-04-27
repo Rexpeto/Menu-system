5e939da995d811c884d634ba782925b3d41b14c7
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-27 14:21:00 -0400

    add response products

--- pages/api/category.js
+++ pages/api/category.js
@@ -2,7 +2,11 @@ import { PrismaClient } from "@prisma/client";
 
 const handler = async (req, res) => {
     const prisma = new PrismaClient();
-    const categoryRes = await prisma.category.findMany();
+    const categoryRes = await prisma.category.findMany({
+        include: {
+            products: true,
+        },
+    });
 
     res.status(200).json(categoryRes);
 };
