d3ea5bb4654a1a012f9149259d5d223c5bcba35f
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-27 12:09:24 -0400

    Title

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Layout from "@/layout/Layout";
+import useStore from "@/hooks/useStore";
 
 const Home = () => {
+    const { categoryActual } = useStore();
     return (
-        <Layout title="Menú">
-            <p>App</p>
+        <Layout title={`Menú ${categoryActual?.name}`}>
+            <h2 className="font-bold text-2xl uppercase">{categoryActual?.name}</h2>
         </Layout>
     );
 };
