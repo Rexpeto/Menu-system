b71a84e8a0986c317af3eefc72941c2067615227
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-27 14:19:55 -0400

    Use products component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Layout from "@/layout/Layout";
 import useStore from "@/hooks/useStore";
+import Products from "@/components/Products";
 
 const Home = () => {
     const { categoryActual } = useStore();
     return (
         <Layout title={`Menú ${categoryActual?.name}`}>
-            <h2 className="font-bold text-2xl uppercase">{categoryActual?.name}</h2>
+            <h2 className="font-bold text-2xl uppercase mb-6">
+                {categoryActual?.name}
+            </h2>
+            <Products products={categoryActual?.products} />
         </Layout>
     );
 };
