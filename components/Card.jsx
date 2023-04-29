cd53453e079530c110582896afe422da7ca57b5f
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-29 14:21:48 -0400

    Open modal

--- components/Card.jsx
+++ components/Card.jsx
@@ -3,19 +3,17 @@ import useStore from "@/hooks/useStore";
 
 const Card = ({ data }) => {
     const { name, price, image } = data;
-    const { handdlerSetProducto } = useStore();
+    const { handdlerSetProducto, handdlerShowModal } = useStore();
 
     return (
         <div className="grid max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-                <Image
-                    className="rounded-t-lg"
-                    src={`/assets/img/${image}.jpg`}
-                    alt=""
-                    width={400}
-                    height={500}
-                />
-            </a>
+            <Image
+                className="rounded-t-lg"
+                src={`/assets/img/${image}.jpg`}
+                alt=""
+                width={400}
+                height={500}
+            />
             <div className="flex flex-col py-5 px-2 max-h-full justify-between">
                 <div>
                     <a href="#">
@@ -29,10 +27,12 @@ const Card = ({ data }) => {
                 </div>
                 <button
                     type="button"
-                    onClick={() => handdlerSetProducto(data)}
+                    onClick={() => {
+                        handdlerSetProducto(data), handdlerShowModal();
+                    }}
                     className="block items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg hover:bg-blue-800 outline-none bg-blue-600 dark:hover:bg-blue-700 transition duration-150"
                 >
-                    Agregar
+                    Ordenar
                 </button>
             </div>
         </div>
