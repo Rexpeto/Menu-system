3b29732f4e883321f96316bc9200e44ca708bd79
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-30 19:35:01 -0400

    Pages ordenes

---
+++ pages/ordenes.js
@@ -0,0 +1,11 @@
+import Layout from "@/layout/Layout";
+
+const Resumen = () => {
+    return (
+        <Layout title="Ordenes">
+            <h1 className="text-4xl font-bold">Ordenes</h1>
+        </Layout>
+    );
+};
+
+export default Resumen;
