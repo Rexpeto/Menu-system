16a79e13d65d57a62ddb7e98c6cd9cd30a44354a
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-30 19:35:16 -0400

    Pages total

---
+++ pages/total.js
@@ -0,0 +1,11 @@
+import Layout from "@/layout/Layout";
+
+const Total = () => {
+    return (
+        <Layout title="Total">
+            <h1 className="text-4xl font-bold">Total</h1>
+        </Layout>
+    );
+};
+
+export default Total;
