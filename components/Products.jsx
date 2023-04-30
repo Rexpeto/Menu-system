da9efa00d52c5bcf7f1cd0a58ce1278548961d92
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-04-30 13:23:22 -0400

    New layout

--- components/Products.jsx
+++ components/Products.jsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 const Products = ({ products }) => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
             {products &&
                 products.map((data) => <Card data={data} key={data?.id} />)}
         </div>
