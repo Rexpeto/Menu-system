a39b70d7279437b2d93a6ee9bd2f84df0d6b198c
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-04 19:14:42 -0400

    Admin kitchen

---
+++ pages/cocina/index.js
@@ -0,0 +1,11 @@
+import LayoutAdmin from "@/layout/LayoutAdmin";
+
+const Index = () => {
+    return (
+        <LayoutAdmin title="Cocina">
+            <div>Cocina</div>
+        </LayoutAdmin>
+    );
+};
+
+export default Index;
