6dd33b5a054effe435c40b9c087d7feba65d397f
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-05-04 20:23:43 -0400

    Use SWR api

--- pages/cocina/index.js
+++ pages/cocina/index.js
@@ -1,10 +1,23 @@
+import useSWR from "swr";
+import axios from "axios";
 import LayoutAdmin from "@/layout/LayoutAdmin";
 
 const Index = () => {
+    const fetcher = () => axios("/api/order").then((datos) => datos.data);
+    const { data, error, isLoading } = useSWR("/api/order", fetcher);
+
     return (
-        <LayoutAdmin title="Cocina">
-            <div>Cocina</div>
-        </LayoutAdmin>
+        <>
+            {isLoading ? (
+                <div className="flex justify-center items-center h-screen">
+                    <span className="loader"></span>
+                </div>
+            ) : (
+                <LayoutAdmin title="Cocina">
+                    <div>Cocina</div>
+                </LayoutAdmin>
+            )}
+        </>
     );
 };
 
